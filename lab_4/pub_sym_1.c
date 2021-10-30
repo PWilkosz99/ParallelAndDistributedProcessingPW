4b42a7531bdeb3883e3b280566bbdcdc526f20e6
PWilkosz99 <pwilkosz99@gmail.com>
2021-10-30 18:49:42 +0200

    After LAB 4

--- lab_4/pub_sym_1.c
+++ lab_4/pub_sym_1.c
@@ -1,78 +1,83 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
 #include <time.h>
-#include <pthread.h>
+#include<pthread.h>
 #include <unistd.h>
 
-#define ILE_MUSZE_WYPIC 3
+#define ILE_MUSZE_WYPIC 2
 
-void *watek_klient(void *arg);
-pthread_mutex_t mutex;
+void * watek_klient (void * arg);
+int liczbaUzywanychKufli=0;
+int l_kf=0;
+  pthread_mutex_t muteks;
 
-int main(void)
-{
+int main( void ){
 
   pthread_t *tab_klient;
   int *tab_klient_id;
-  pthread_mutex_init(&mutex, NULL);
 
-  int l_kl, l_kf, l_kr, i;
+  pthread_mutex_init( &muteks, NULL);
 
-  printf("\nLiczba klientow: ");
-  scanf("%d", &l_kl);
+  int l_kl, l_kr, i;
 
-  printf("\nLiczba kufli: ");
-  scanf("%d", &l_kf);
+  printf("\nLiczba klientow: "); scanf("%d", &l_kl);
+
+  printf("\nLiczba kufli: "); scanf("%d", &l_kf);
 
   //printf("\nLiczba kranow: "); scanf("%d", &l_kr);
   l_kr = 1;
 
-  tab_klient = (pthread_t *)malloc(l_kl * sizeof(pthread_t));
-  tab_klient_id = (int *)malloc(l_kl * sizeof(int));
-  for (i = 0; i < l_kl; i++)
-    tab_klient_id[i] = i;
+  tab_klient = (pthread_t *) malloc(l_kl*sizeof(pthread_t));
+  tab_klient_id = (int *) malloc(l_kl*sizeof(int));
+  for(i=0;i<l_kl;i++) tab_klient_id[i]=i;
+
 
   printf("\nOtwieramy pub (simple)!\n");
-  printf("\nLiczba wolnych kufli %d\n", l_kf);
+  printf("\nLiczba wolnych kufli %d\n", l_kf); 
 
-  for (i = 0; i < l_kl; i++)
-  {
-    pthread_create(&tab_klient[i], NULL, watek_klient, &tab_klient_id[i]);
+  for(i=0;i<l_kl;i++){
+    pthread_create(&tab_klient[i], NULL, watek_klient, &tab_klient_id[i]); 
   }
-  for (i = 0; i < l_kl; i++)
-  {
-    pthread_join(tab_klient[i], NULL);
+  for(i=0;i<l_kl;i++){
+    pthread_join( tab_klient[i], NULL);
   }
   printf("\nZamykamy pub!\n");
+
 }
 
-void *watek_klient(void *arg_wsk)
-{
 
-  int moj_id = *((int *)arg_wsk);
+void * watek_klient (void * arg_wsk){
+
+  int moj_id = * ((int *)arg_wsk);
 
   int i, j, kufel, result;
   int ile_musze_wypic = ILE_MUSZE_WYPIC;
 
-  printf("\nKlient %d, wchodzę do pubu\n", moj_id);
+  printf("\nKlient %d, wchodzę do pubu\n", moj_id); 
+  pthread_mutex_lock( &muteks);
+  if(liczbaUzywanychKufli<l_kf){
+	  liczbaUzywanychKufli++;
+    
+  for(i=0; i<ile_musze_wypic; i++){
 
-  pthread_mutex_lock(&mutex);
-  for (i = 0; i < ile_musze_wypic; i++)
-  {
-    printf("\nKlient %d, wybieram kufel\n", moj_id);
-
-    j = 0;
-    printf("\nKlient %d, nalewam z kranu %d\n", moj_id, j);
-    usleep(30);
-
-    printf("\nKlient %d, pije\n", moj_id);
+	
+    printf("\nKlient %d, wybieram kufel\n", moj_id); 
+    
+    j=0;
+    printf("\nKlient %d, nalewam z kranu %d\n", moj_id, j); 
+    usleep(10);
+    
+    printf("\nKlient %d, pije\n", moj_id); 
     nanosleep((struct timespec[]){{0, 50000000L}}, NULL);
-
-    printf("\nKlient %d, odkladam kufel\n", moj_id);
+    
+    printf("\nKlient %d, odkladam kufel\n", moj_id); 
+    liczbaUzywanychKufli--;    
   }
-  pthread_mutex_unlock(&mutex);
-
-  printf("\nKlient %d, wychodzę z pubu\n", moj_id);
-
-  return (NULL);
+    pthread_mutex_unlock(&muteks);
 }
+  printf("\nKlient %d, wychodzę z pubu\n", moj_id); 
+    
+  return(NULL);
+} 
+
+
--- lab_4/pub_sym_2.c
+++ lab_4/pub_sym_2.c
@@ -4,46 +4,35 @@
 #include<pthread.h>
 #include <unistd.h>
 
-#define ILE_MUSZE_WYPIC 3
-
-struct struktura_t { 
-  int l_wkf;
-  pthread_mutex_t *tab_kuf; 
-  int l_kr;
-  pthread_mutex_t *tab_kran;  
-};
-struct struktura_t pub_wsk;
-
+#define ILE_MUSZE_WYPIC 2
 
 void * watek_klient (void * arg);
 
+int l_kf, mutex;
+pthread_mutex_t mp = PTHREAD_MUTEX_INITIALIZER;
+
 int main( void ){
+
   pthread_t *tab_klient;
   int *tab_klient_id;
 
-  int l_kl, l_kf, l_kr, i;
+  int l_kl, l_kr, i;
 
   printf("\nLiczba klientow: "); scanf("%d", &l_kl);
 
   printf("\nLiczba kufli: "); scanf("%d", &l_kf);
-  pub_wsk.l_wkf = l_kf;
 
-
-  printf("\nLiczba kranow: "); scanf("%d", &l_kr);
-  pub_wsk.l_kr = l_kr;
+  //printf("\nLiczba kranow: "); scanf("%d", &l_kr);
+  l_kr = 1;
 
   tab_klient = (pthread_t *) malloc(l_kl*sizeof(pthread_t));
   tab_klient_id = (int *) malloc(l_kl*sizeof(int));
   for(i=0;i<l_kl;i++) tab_klient_id[i]=i;
 
-  pub_wsk.tab_kuf = (pthread_mutex_t *) malloc(l_kf*sizeof(pthread_mutex_t));
-  for(i=0;i<l_kf;i++) pthread_mutex_init(&pub_wsk.tab_kuf[i], NULL);
-
-  pub_wsk.tab_kran = (pthread_mutex_t *) malloc(l_kr*sizeof(pthread_mutex_t));
-  for(i=0;i<l_kr;i++) pthread_mutex_init(&pub_wsk.tab_kran[i], NULL);
 
   printf("\nOtwieramy pub (simple)!\n");
   printf("\nLiczba wolnych kufli %d\n", l_kf); 
+  mutex = pthread_mutex_init(&mp, NULL);
 
   for(i=0;i<l_kl;i++){
     pthread_create(&tab_klient[i], NULL, watek_klient, &tab_klient_id[i]); 
@@ -52,6 +41,8 @@ int main( void ){
     pthread_join( tab_klient[i], NULL);
   }
   printf("\nZamykamy pub!\n");
+  printf("\nLiczba kufli: %d\n", l_kf);
+
 
 
 }
@@ -61,42 +52,54 @@ void * watek_klient (void * arg_wsk){
 
   int moj_id = * ((int *)arg_wsk);
 
-  int i, i_wypite, kran, kufel, result;
-
+  int i, j, kufel, result;
   int ile_musze_wypic = ILE_MUSZE_WYPIC;
 
   printf("\nKlient %d, wchodzę do pubu\n", moj_id); 
     
-  for(i_wypite=0; i_wypite<ile_musze_wypic; i_wypite++){
+  for(i=0; i<ile_musze_wypic; i++){
+    while(1){
+      printf("\nKlient %d, wybieram kufel. Ilosc: %d\n", moj_id,l_kf);
+     
+      if(pthread_mutex_trylock(&mutex) == 0){
+	if(l_kf <= 0){
+	  printf("Klient %d  Brak kufli\n", moj_id);
+	  pthread_mutex_unlock(&mutex);
+	  sleep(2);
+	}else{
+	  
+	  l_kf -= 1;
+	  printf("Klient %d  Pobral kufel. Ilosc: %d\n", moj_id, l_kf);
+	  pthread_mutex_unlock(&mutex);
+	  break;
+	}
+      }
+    }
 
-    printf("\nKlient %d, wybieram kufel\n", moj_id); 
-
-    // ???????????
-
-    // printf("\nKlient %d, wybrałem kufel %d\n", moj_id, kufel); ?????
-
-    printf("\nKlient %d, wybieram kran\n", moj_id); 
-
-    // ????????????
+    j=0;
+    printf("\nKlient %d, nalewam z kranu %d\n", moj_id, j); 
+    usleep(30);
     
-    printf("\nKlient %d, nalewam z kranu %d\n", moj_id, kran); 
-    usleep(100);
+    printf("\nKlient %d, pije\n", moj_id); 
+    usleep(30);
 
-    // ????????????
-    if(kran==0)  printf("\nKlient %d, pije piwo Guinness\n", moj_id); 
-    else if(kran==1)  printf("\nKlient %d, pije piwo Żywiec\n", moj_id); 
-    else if(kran==2)  printf("\nKlient %d, pije piwo Heineken\n", moj_id); 
-    else if(kran==3)  printf("\nKlient %d, pije piwo Okocim\n", moj_id); 
-    else if(kran==4)  printf("\nKlient %d, pije piwo Karlsberg\n", moj_id); 
-    // itd., itp. 
-
-    usleep(100);
-
-    // ????????????
-
-    printf("\nKlient %d, odkladam kufel %d\n", moj_id, kufel); 
-
+    while(1){
+      if(pthread_mutex_trylock(&mutex) == 0){
+	l_kf +=  1;
+	pthread_mutex_unlock(&mutex);
+	printf("Klient %d Oddalem kufel. Stan: %d\n", moj_id, l_kf);
+	break;
+      }else{
+	printf("Klient %d Nie moge oddac kufla\n", moj_id);
+	usleep(10);
+      }
+    }
+    
   }
 
+  printf("\nKlient %d, wychodzę z pubu\n", moj_id); 
+    
   return(NULL);
 } 
+
+
