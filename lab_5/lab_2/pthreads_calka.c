1ecf3687f0e59cc75395b32035b08bfdb8ef8558
PWilkosz99LU <pwilkosz99@gmail.com>
2021-11-07 16:17:05 +0100

    LAB5 release

--- lab_5/lab_2/pthreads_calka.c
+++ lab_5/lab_2/pthreads_calka.c
@@ -5,11 +5,15 @@
 #include "../../pomiar_czasu/pomiar_czasu.h"
 
 double funkcja(double x);
+
 double funkcja(double x) { return (sin(x)); }
 
 double calka_sekw(double a, double b, double dx);
-double calka_zrownoleglenie_cyk(double a, double b, double dx);
-double calka_zrownoleglenie_blk(double a, double b, double dx);
+
+double calka_zrownoleglenie_petli_cykl(double a, double b, double dx);
+
+double calka_zrownoleglenie_petli_blok(double a, double b, double dx);
+
 double calka_dekompozycja_obszaru(double a, double b, double dx);
 
 #define LICZBA_W_MAX 1000
@@ -20,8 +24,11 @@ int l_w_global = 0;
 
 int main(int argc, char *argv[])
 {
+
   int i;
-  double t1, t2, t3;
+
+  double t1, t2, t3; // po co t2, t3 ??
+
   double a, b, dx, calka;
 
   printf("\nProgram obliczania całki z funkcji (sinus) metodą trapezów.\n");
@@ -32,7 +39,7 @@ int main(int argc, char *argv[])
   b = M_PI;
   //printf("\nPodaj prawy kraniec przedziału całkowania: "); scanf("%lf", &b);
 
-  printf("\nPodaj wysokości pojedynczego trapezu:  ");
+  printf("\nPodaj dzielnik wysokości (1/x) pojedynczego trapezu:  ");
   scanf("%lf", &dx);
   dx = 1.0 / dx;
   printf("%lf\n", dx);
@@ -53,7 +60,7 @@ int main(int argc, char *argv[])
   printf("\nPoczatek obliczeń równoległych (zrównoleglenie pętli cykliczne)\n");
   t1 = czas_zegara();
 
-  calka = calka_zrownoleglenie_cyk(a, b, dx);
+  calka = calka_zrownoleglenie_petli_cykl(a, b, dx);
 
   t1 = czas_zegara() - t1;
   printf("\nKoniec obliczen  równoległych (zrównoleglenie pętli cykliczne) \n");
@@ -62,7 +69,7 @@ int main(int argc, char *argv[])
   printf("\nPoczatek obliczeń równoległych (zrównoleglenie pętli blokowe)\n");
   t1 = czas_zegara();
 
-  calka = calka_zrownoleglenie_blk(a, b, dx);
+  calka = calka_zrownoleglenie_petli_blok(a, b, dx);
 
   t1 = czas_zegara() - t1;
   printf("\nKoniec obliczen  równoległych (zrównoleglenie pętli blokowe) \n");
@@ -108,7 +115,7 @@ static int N_global;
 
 void *calka_fragment_petli_w_cykl(void *arg_wsk);
 
-double calka_zrownoleglenie_cyk(double a, double b, double dx)
+double calka_zrownoleglenie_petli_cykl(double a, double b, double dx)
 {
   int N = ceil((b - a) / dx);
   calka_global = 0;
@@ -120,10 +127,10 @@ double calka_zrownoleglenie_cyk(double a, double b, double dx)
   //printf("\nPodaj liczbę wątków:  "); scanf("%d", &l_w);
 
   // tworzenie struktur danych do obsługi wielowątkowości
-  int data_str[l_w];
+  int indeksy[l_w];
   for (int i = 0; i < l_w; i++)
   {
-    data_str[i] = i;
+    indeksy[i] = i;
   }
 
   pthread_t watki[l_w];
@@ -137,7 +144,7 @@ double calka_zrownoleglenie_cyk(double a, double b, double dx)
   for (int i = 0; i < l_w; i++)
   {
     global_array_of_local_sums[i] = 0.0;
-    pthread_create(&watki[i], NULL, calka_fragment_petli_w_cykl, (void *)&data_str[i]);
+    pthread_create(&watki[i], NULL, calka_fragment_petli_w_cykl, (void *)&indeksy[i]);
   }
 
   // oczekiwanie na zakończenie pracy wątków
@@ -154,8 +161,9 @@ void *calka_fragment_petli_w_cykl(void *arg_wsk)
 {
   int my_id = *((int *)arg_wsk);
 
-  double a, b, dx;
-  int N, l_w;
+  double a, b, dx; // skąd pobrać dane a, b, dx, N, l_w ?
+  int N, l_w;      // wariant 1 - globalne
+  // a = a_global; // itd. itp. - wartości globalne nadaje calka_zrownoleglenie_petli
 
   l_w = l_w_global;
   a = a_global;
@@ -172,6 +180,8 @@ void *calka_fragment_petli_w_cykl(void *arg_wsk)
   //int my_end = 0;
   //int my_stride = 0;
 
+  // something else ? (dekompozycja blokowo-cykliczna)
+
   dx = (b - a) / N;
 
   my_start = a + my_id;
@@ -187,6 +197,7 @@ void *calka_fragment_petli_w_cykl(void *arg_wsk)
 
   for (i = my_start; i < my_end; i += my_stride)
   {
+
     double x1 = a + i * dx;
     calka += 0.5 * dx * (funkcja(x1) + funkcja(x1 + dx));
     //printf("i %d, x1 %lf, funkcja(x1) %lf, całka = %.15lf\n",
@@ -198,7 +209,7 @@ void *calka_fragment_petli_w_cykl(void *arg_wsk)
 
 void *calka_fragment_petli_w_blok(void *arg_wsk);
 
-double calka_zrownoleglenie_blk(double a, double b, double dx)
+double calka_zrownoleglenie_petli_blok(double a, double b, double dx)
 {
   int N = ceil((b - a) / dx);
   calka_global = 0;
@@ -210,10 +221,10 @@ double calka_zrownoleglenie_blk(double a, double b, double dx)
   //printf("\nPodaj liczbę wątków:  "); scanf("%d", &l_w);
 
   // tworzenie struktur danych do obsługi wielowątkowości
-  int data_str[l_w];
+  int indeksy[l_w];
   for (int i = 0; i < l_w; i++)
   {
-    data_str[i] = i;
+    indeksy[i] = i;
   }
 
   pthread_t watki[l_w];
@@ -227,7 +238,7 @@ double calka_zrownoleglenie_blk(double a, double b, double dx)
   for (int i = 0; i < l_w; i++)
   {
     global_array_of_local_sums[i] = 0.0;
-    pthread_create(&watki[i], NULL, calka_fragment_petli_w_blok, (void *)&data_str[i]);
+    pthread_create(&watki[i], NULL, calka_fragment_petli_w_blok, (void *)&indeksy[i]);
   }
 
   // oczekiwanie na zakończenie pracy wątków
@@ -278,6 +289,10 @@ void *calka_fragment_petli_w_blok(void *arg_wsk)
 
   for (i = my_start; i <= my_end && i < N; i++)
   {
+    // if (0 == i % my_stride)
+    // {
+    //   printf("%d\n", i);
+    // }
     double x1 = a + i * dx;
     calka += 0.5 * dx * (funkcja(x1) + funkcja(x1 + dx));
     //printf("i %d, x1 %lf, funkcja(x1) %lf, całka = %.15lf\n",
@@ -287,9 +302,7 @@ void *calka_fragment_petli_w_blok(void *arg_wsk)
   global_array_of_local_sums[my_id] = calka;
 }
 
-void *calka_podobszar_w(void *arg_wsk);
-
-typedef struct dek_str
+typedef struct calka_dekompozycja_data
 {
   double a;
   double b;
@@ -297,17 +310,24 @@ typedef struct dek_str
   double result;
   int id;
   int n;
-} dek_str;
+} calka_dekompozycja_data;
+
+void *calka_podobszar_w(void *arg_wsk);
 
 double calka_dekompozycja_obszaru(double a, double b, double dx)
 {
   calka_global = 0;
+  //printf("a %lf, b %lf, dx %lf\n", a, b, dx);
+
   int l_w = l_w_global;
+  //printf("\nPodaj liczbę wątków:  "); scanf("%d", &l_w);
 
   double calka_suma_local = 0.0;
 
-  dek_str *data_str = (dek_str *)malloc(sizeof(dek_str) * l_w);
-  
+  // tworzenie struktur danych do obsługi wielowątkowości
+
+  calka_dekompozycja_data *indeksy = (calka_dekompozycja_data *)malloc(sizeof(calka_dekompozycja_data) * l_w);
+
   pthread_t watki[l_w];
 
   // tworzenie wątków
@@ -323,29 +343,40 @@ double calka_dekompozycja_obszaru(double a, double b, double dx)
     int my_stride = 0;
     int N = ceil((b - a) / dx);
 
-    data_str[i].dx = (b - a) / N;
+    dx = (b - a) / N;
 
     my_stride = N / l_w + 1;
-    data_str[i].a = my_id * my_stride + 1;
+    my_start = my_id * my_stride + 1;
 
     if (my_id == 0)
     {
-      data_str[i].a = 0;
+      my_start = 0;
     }
+    my_end = (my_id + 1) * my_stride;
 
-    data_str[i].b = (my_id + 1) * my_stride;
-    data_str[i].result = 0;
-    data_str[i].id = i;
-    data_str[i].n = N;
+    indeksy[i].a = a;
+    indeksy[i].b = b;
+    indeksy[i].dx = dx;
+    indeksy[i].result = 0;
+    indeksy[i].id = i;
+    indeksy[i].n = N;
 
-    pthread_create(&watki[i], NULL, calka_podobszar_w, (void *)&data_str[i]);
+    pthread_create(&watki[i], NULL, calka_podobszar_w, (void *)&indeksy[i]);
   }
 
   // oczekiwanie na zakończenie pracy wątków
   for (int i = 0; i < l_w; i++)
   {
-    pthread_join(watki[i], NULL);
-    calka_suma_local += data_str[i].result;
+    void *addr;
+
+    pthread_join(watki[i], &addr);
+    calka_suma_local += indeksy[i].result;
+
+    // double dbl = *(double *)addr;
+
+    // printf("%lf\n",*(double*)addr);
+
+    //calka_suma_local += dbl;
   }
 
   return (calka_suma_local);
@@ -354,35 +385,42 @@ double calka_dekompozycja_obszaru(double a, double b, double dx)
 void *calka_podobszar_w(void *arg_wsk)
 {
 
-  dek_str *data_pod_w = (dek_str *)arg_wsk;
+  calka_dekompozycja_data *my_data = (calka_dekompozycja_data *)arg_wsk;
 
-  double a_local, b_local, dx;
-  // rozpakowanie danych przesłanych do wątku
+  int N_local = my_data->n;
+  double dx_adjust_local = (my_data->b - my_data->a) / N_local;
   
-  dx = data_pod_w->dx;
-  a_local = data_pod_w->a;
-  b_local = data_pod_w->b;
+  int my_start = 0;
+  int my_end = 0;
+  int my_stride = 0;
 
+  int N = N_global;
+  double dx = (my_data->a - my_data->b) / N;
 
-  //int N_local = ceil((b_local - a_local) / dx);
-  int N_local = data_pod_w->n;
-  //double dx_adjust_local = (b_local - a_local) / N_local;
-  double dx_adjust_local = dx;
+  my_stride = N / l_w_global + 1;
+  my_start = my_data->id * my_stride + 1;
 
-  printf("\nWątek %d\n", data_pod_w->id);
+  if (my_data->id == 0)
+  {
+    my_start = 0;
+  }
+  my_end = (my_data->id + 1) * my_stride;
+
+  printf("\nWątek %d\n", my_data->id);
   printf("a_local %lf, b_local %lf, dx_adjust_local %lf, n_local %d\n",
-         a_local, b_local, dx_adjust_local, N_local);
+         my_data->a, my_data->b, dx_adjust_local, N_local);
 
   int i;
-  double calka = 0.0;
-  for (i = 0; i < N_local / l_w_global; i++)
+  double *calka = (double *)malloc(sizeof(double));
+  double x1 = my_data->a + my_start * dx;
+
+  double a = funkcja(x1);
+  for (i = my_start; i <= my_end && i < N; i++)
   {
-
-    double x1 = a_local + i * dx_adjust_local;
-    calka += 0.5 * dx_adjust_local * (funkcja(x1) + funkcja(x1 + dx_adjust_local));
-    //printf("i %d, x1 %lf, funkcja(x1) %lf, całka = %.15lf\n",
-    //	   i, x1, funkcja(x1), calka);
+    x1 = my_data->a + i * dx;
+    double b = funkcja(x1 + dx);
+    *calka += 0.5 * dx * (a + b);
+    a = b;
   }
-
-  data_pod_w->result = calka;
+  my_data->result = *calka;
 }
