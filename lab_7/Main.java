aee93f85b6957e1948cb7b4c825d896a0ce9daf9
PWilkosz99LU <pwilkosz99@gmail.com>
2021-11-22 19:53:10 +0100

    Executor import - int PI

--- lab_7/Main.java
+++ lab_7/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //seq
-        Calka_callable calka = new Calka_callable(0, 100, 0.1);
+        Calka_callable calka = new Calka_callable(0, Math.PI, 0.1);
         var res1 = calka.compute();
 
         System.out.println(res1);
