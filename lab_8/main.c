8ed2abbb44536fd08709b67c57c3296d07b9c250
PWilkosz99LU <pwilkosz99@gmail.com>
2021-12-05 00:34:11 +0100

    Barrier POSIX implementation

--- lab_8/main.c
+++ lab_8/main.c
@@ -1,6 +1,6 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 extern void bariera_init(int);
 extern void bariera(void);
@@ -10,50 +10,82 @@ extern void bariera(void);
 
 pthread_t watki[LICZBA_W];
 
-void *cokolwiek( void *arg);
+void *cokolwiek(void *arg);
 
-int main( int argc, char *argv[] ){
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-  int i, indeksy[LICZBA_W]; for(i=0;i<LICZBA_W;i++) indeksy[i]=i; 
+int BarrierCounter = 0;
+
+void bariera_init(int a)
+{
+}
+
+void bariera(void)
+{
+  pthread_mutex_lock(&mutex);
+  if (BarrierCounter == LICZBA_W - 1)
+  {
+    printf("\tZwolnienie. Wejsc w bariere: %d\n", (BarrierCounter + 1));
+    pthread_cond_broadcast(&cond);
+    BarrierCounter = 0;
+  }
+  else
+  {
+    BarrierCounter++;
+    pthread_cond_wait(&cond, &mutex);
+  }
+
+  pthread_mutex_unlock(&mutex);
+}
+
+int main(int argc, char *argv[])
+{
+
+  int i, indeksy[LICZBA_W];
+  for (i = 0; i < LICZBA_W; i++)
+    indeksy[i] = i;
 
   // use of pthread_once (once_control, init_routine) ?
   // int pthread_once(pthread_once_t *once_control, void (*init_routine)(void));
   // but init_routine has no arguments...
   bariera_init(LICZBA_W);
 
-  for(i=0; i<LICZBA_W; i++ ) {
-    pthread_create( &watki[i], NULL, cokolwiek, (void *) &indeksy[i] );
+  for (i = 0; i < LICZBA_W; i++)
+  {
+    pthread_create(&watki[i], NULL, cokolwiek, (void *)&indeksy[i]);
   }
 
-  for(i=0; i<LICZBA_W; i++ ) pthread_join( watki[i], NULL );
+  for (i = 0; i < LICZBA_W; i++)
+    pthread_join(watki[i], NULL);
 
-  pthread_exit( NULL);  
+  pthread_exit(NULL);
 }
 
-
-void *cokolwiek( void *arg){
+void *cokolwiek(void *arg)
+{
 
   int i, moj_id;
 
-  moj_id = *( (int *) arg ); 
+  moj_id = *((int *)arg);
 
-  printf("przed bariera 1 - watek %d\n",moj_id);
+  printf("przed bariera 1 - watek %d\n", moj_id);
 
   bariera();
 
-  //  printf("przed bariera 2 - watek %d\n",moj_id);
+  printf("przed bariera 2 - watek %d\n", moj_id);
 
-  //bariera();
+  bariera();
 
-  //printf("przed bariera 3 - watek %d\n",moj_id);
+  printf("przed bariera 3 - watek %d\n", moj_id);
 
-  //bariera();
+  bariera();
 
-  //printf("przed bariera 4 - watek %d\n",moj_id);
+  printf("przed bariera 4 - watek %d\n", moj_id);
 
-  //bariera();
+  bariera();
 
-  printf("po ostatniej barierze - watek %d\n",moj_id);  
+  printf("po ostatniej barierze - watek %d\n", moj_id);
 
-  pthread_exit( (void *)0);
+  pthread_exit((void *)0);
 }
