50b2837354e1e49d3a7506a84143fdb57340de22
PWilkosz99U <pwilkosz99@gmail.com>
2021-12-09 21:17:14 +0100

    Simple loop with shared variable

--- lab_9/lab_9_simple/openmp_petle_simple.c
+++ lab_9/lab_9_simple/openmp_petle_simple.c
@@ -5,36 +5,41 @@
 
 #define WYMIAR 18
 
-int main ()
+int main()
 {
+  setenv("OMP_NUM_THREADS", "4", 1);
   double a[WYMIAR];
-  int n,i;
+  int n, i;
 
-  for(i=0;i<WYMIAR;i++) a[i]=1.02*i;
+  for (i = 0; i < WYMIAR; i++)
+    a[i] = 1.02 * i;
 
-  n=WYMIAR;
+  n = WYMIAR;
 
   // pętla sekwencyjna
-  double suma=0.0;
-  for(i=0;i<WYMIAR;i++) {
-      suma += a[i];
+  double suma = 0.0;
+  for (i = 0; i < WYMIAR; i++)
+  {
+    suma += a[i];
   }
-  
+
   printf("Suma wyrazów tablicy: %lf\n", suma);
 
   // pętla do modyfikacji - docelowo równoległa w OpenMP
-  double suma_parallel=0.0;
-  // ...
-  for(i=0;i<WYMIAR;i++) {
-    int id_w = omp_get_thread_num();
-    // ...
+  double suma_parallel = 0.0;
+#pragma omp parallel default(none) shared(a, suma_parallel)
+  {
+#pragma omp for ordered
+    for (i = 0; i < WYMIAR; i++)
+    {
+      int id_w = omp_get_thread_num();
+#pragma omp critical
       suma_parallel += a[i];
       // ...
 #pragma omp ordered
-      printf("a[%2d]->W_%1d  \n",i,id_w); 
+      printf("a[%2d]->W_%1d  \n", i, id_w);
+    }
   }
-
   printf("\nSuma wyrazów tablicy równolegle (z klauzulą - ....: %lf\n",
-	 suma_parallel);
-
+         suma_parallel);
 }
